12d432d3eb1ff5bbd9299137602b7eb52aa74157
Theo POTILLION <tpotilli@bess-f2r6s3.clusters.42paris.fr>
2024-05-23 10:51:29 +0200

    EX00 finis

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -5,34 +5,31 @@
 
 int main(int argc, char **argv)
 {
-	std:: string str;
 	int i;
 	int count = 1;
+	char	c;
 
 	if (argc == 1)
-		std:: cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		std:: cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else if (argc > 1)
 	{
-		while (argc > count)
+		while (count < argc)
 		{
 			i = 0;
-			while (i < (int)strlen(argv[count]))
+			while (argv[count][i])
 			{
 				if (argv[count][i] >= 97 && argv[count][i] <= 122)
 				{
-					str[i] = argv[count][i];
-					str[i] = str[i] - (char)32;
+					c = argv[count][i] - 32;
+					std::cout << c;
 				}
+				else
+					std::cout << argv[count][i];
 				i++;
-			}
-			int j = 0;
-			while (j < i)
-			{
-				std :: cout << str[j];
-				j++;
 			}
 			count++;
 		}
 	}
-	std:: cout << std::endl;
+	std::cout << std::endl;
+	return (0);
 }
