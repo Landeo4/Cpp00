4f7a2f7bb12d2f875cd326df994331863361a517
Landeo4 <theo91.pot@gmail.com>
2024-05-24 18:48:34 +0200

    c++

---
+++ ex01/Contact.cpp
@@ -0,0 +1,11 @@
+#include "Contact.hpp"
+
+Contact::Contact(void){
+	std::cout << "Constructor called" << std::endl;
+	return;
+}
+
+Contact::~Contact(void){
+	std::cout << "Destructor called" << std::endl;
+	return;
+}
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,8 +5,22 @@
 
 int main()
 {
-	std::string input;
+	std::string x;
 
-	input = std::cin.get();
-	std:: cout << input << std::endl;
+
+	while (42)
+	{
+		std::cout << "please enter an input" << std::endl;
+		std::cin >> x;
+		if (x == "ADD")
+			ft_add_contact();
+		if (x == "SEARCH")
+			
+		if (x == "EXIT")
+			return 0;
+		else
+			std::cout << "wrong input" << std::endl;
+	}
+	std:: cout << x << std::endl;
 }
+// essayer de trouver comment rendre cette excercice plus "c++"
